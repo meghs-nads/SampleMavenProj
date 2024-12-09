7fd6ed42d68b16d1bee3b08b606c5ded995a695c
meghs-nads <meghananadella871@gmail.com>
2024-12-09 20:28:01 +0530

    second change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        int a = 5 , b = 7 ;
-        System.out.println(a+b);
+        System.out.println("Meghana");
     }
 }
