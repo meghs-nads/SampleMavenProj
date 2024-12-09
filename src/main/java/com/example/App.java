8e88e70d317f10ecb8a6bcd02f1205599ce7fa31
meghs-nads <meghananadella871@gmail.com>
2024-12-09 20:20:19 +0530

    first change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int a = 5 , b = 7 ;
+        System.out.println(a+b);
     }
 }
